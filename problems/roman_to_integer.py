5157eb9e9138537be75af748dd2fb156ff4cc7b5
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-04-29 19:24:15 -0400

    [IMP] Enhance roman to integers code.

--- problems/roman_to_integer.py
+++ problems/roman_to_integer.py
@@ -1,19 +1,9 @@
-ROMAN_NUMERALS = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-}
-ORDERS = list(ROMAN_NUMERALS.keys())
-
+ROMAN_NUMERALS = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
 class Solution:
     def romanToInt(self, s: str):
         result = index = 0
         while index < len(s):
-            if index < len(s)-1 and ORDERS.index(s[index]) < ORDERS.index(s[index + 1]):
+            if index < len(s) - 1 and ROMAN_NUMERALS.get(s[index]) < ROMAN_NUMERALS.get(s[index + 1]):
                 result += ROMAN_NUMERALS.get(s[index + 1]) - ROMAN_NUMERALS.get(s[index])
                 index += 1
             else:
