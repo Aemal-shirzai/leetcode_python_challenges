b72a32f1e9036cd8653c62b74536a1122822b2c2
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-05-09 00:23:12 -0400

    [IMP] Add second solution to remov duplicate from sorted array.

--- problems/26.remove_duplicates_from_sorted_array.py
+++ problems/26.remove_duplicates_from_sorted_array.py
@@ -7,3 +7,20 @@ class Solution:
                 nums[to_be_replace_index] = nums[i]
                 to_be_replace_index += 1
         return to_be_replace_index
+    
+
+        # Solution 2: Slow
+        # unique_list = []
+        # counter = 0
+        # numbe_of_iterations = len(set(nums))
+        # while counter < numbe_of_iterations:
+        #     element = nums[counter]
+        #     if element not in unique_list:
+        #         unique_list.append(element)
+        #         counter += 1
+        #         continue  
+        #     nums.pop(counter)
+        #     nums.append('_')
+        # return counter
+    
+
