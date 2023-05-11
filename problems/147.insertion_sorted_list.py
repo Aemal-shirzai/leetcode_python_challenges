1a3e6769543f2f1cc8e391d278b4ab3803e88ec3
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-05-10 22:26:37 -0400

    [IMP] Add test case to Insertion sorted List Problem.

--- problems/147.insertion_sorted_list.py
+++ problems/147.insertion_sorted_list.py
@@ -7,3 +7,37 @@ class ListNode:
 
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        def return_list_values(node, result=None):
+            if not result: result = []
+            if node == None: return result
+            result.append(node.val)
+            return_list_values(node.next, result=result)
+            return result
+        
+        single_list = return_list_values(head)
+        single_list.sort()
+       
+        # Create new node
+        head = ListNode(None)
+        current = head
+        for item in single_list:
+            current.next = ListNode(item)
+            current = current.next
+        head = head.next
+        return head
+    
+# Test Case
+  
+# head = ListNode(-1)
+# node2 = ListNode(5)
+# node3 = ListNode(3)
+# node4 = ListNode(4)
+# node5 = ListNode(0)
+# head.next = node2
+# node2.next = node3
+# node3.next = node4
+# node4.next = node5
+
+# obj = Solution()
+# obj.mergeKLists(head)
