2e10541f35bfe69f04ad6ce31e14f3f2c7348b6d
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-05-12 22:33:37 -0400

    [IMP] Solve Search index position problem.

---
+++ problems/35.search_index_position.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i, num in enumerate(nums):
+            if num == target or num > target:
+                return i    
+        return len(nums)
