81f242c2b2d852777c6d56f9e0399362af198590
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-05-18 15:06:03 -0400

    [IMP] Add effecient solution to single Number problem.

--- problems/136.single_number.py
+++ problems/136.single_number.py
@@ -3,4 +3,11 @@ from typing import List
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return next(filter(lambda num: nums.count(num) == 1, nums))
-        
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res ^= num
+
+        return res
