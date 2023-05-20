ada32f34e7d67f49521cc5b76f906248d793c975
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-05-20 13:23:21 -0400

    [IMP] solve merge sorted array problem.

---
+++ problems/88.merge_sorted_array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        for index in range(m, m + n):
+            nums1[index] = nums2[index - m]
+        nums1.sort()
