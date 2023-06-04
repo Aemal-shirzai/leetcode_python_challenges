1413eafcd85c7ff519cfb0e7fdf1ad9651940425
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-06-03 20:52:22 -0400

    [IMP] Add sulotion for height of tree.

---
+++ problems/Tree_others/9.height_of_tree.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution():
+    def heightOfNodes(self, root: TreeNode) -> int:
+        
+        def traverse(node):
+            if not node:
+                return 0
+            
+            left_node = traverse(node.left)
+            right_node = traverse(node.right)
+
+            return max(left_node, right_node) + 1
+
+        return traverse(root)
