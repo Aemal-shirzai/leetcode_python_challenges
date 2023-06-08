0c28383b3a679882e861368831c683acf590f48f
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-06-07 23:04:07 -0400

    [IMP] Solve Same tree problem.

---
+++ problems/100.same_tree.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        def isIdentical(p, q):
+            if not p and not q:
+                return True
+            
+            if (not p or not q) or (p.val != q.val):
+                return False
+            
+            return isIdentical(p.left, q.left) and isIdentical(p.right, q.right)
+
+
+        return isIdentical(p, q)
